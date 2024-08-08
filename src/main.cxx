f97cb48c7f045d2b8eba301eb56e56857d43cc06
n-inelie <nin3li3@yahoo.com>
2024-08-08 08:36:57 +0000

    draw triangle

--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,21 @@
-#include "callbacks.hxx"
 #include <iostream>
 
+#define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
+#include "callbacks.hxx"
+#include "shader.hxx"
+
 #define WIDTH 800
 #define HEIGHT 500
+
+float vertices[] = {
+    // positions         // colors
+    0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+    0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f  // top
+};
 
 int main(void) {
     std::cout << "Spark Ignited\n";
@@ -34,15 +44,38 @@ int main(void) {
 
     window_set_all_callbacks(window);
 
-    double time = glfwGetTime();
+    Shader shader("shaders/basic.vert.glsl", "shaders/basic.frag.glsl");
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glClearColor(1.0f, 0.5f, 1.0f, 1.0f);
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwDestroyWindow(window);
     glfwTerminate();
